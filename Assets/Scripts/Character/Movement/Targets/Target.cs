5ac544acb8fe6173bf5d023309a1c0e6c653649b
DragonHeart000 <rileyshumway@gmail.com>
2020-07-10 17:28:13 -0500

    Added targets and target hub that can be placed in levels

---
+++ Assets/Scripts/Character/Movement/Targets/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public bool isEnabled = true; //If the target is something the player should consider going to.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+    }
+}
