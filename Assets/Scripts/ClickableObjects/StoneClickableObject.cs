552d99743c49e617478aa1312bd88bca0173f46c
DragonHeart000 <rileyshumway@gmail.com>
2020-07-11 14:02:10 -0500

    Started adding clickable rocks to level 1 and fixed collision issue with dragging a rock onto the player

--- Assets/Scripts/ClickableObjects/StoneClickableObject.cs
+++ Assets/Scripts/ClickableObjects/StoneClickableObject.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StoneClickableObject : ClickableObject
 {
 
     bool IsBeingDraged = false;
     Camera mainCamera;
+
+    public NavMeshObstacle thisObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,10 @@ public class StoneClickableObject : ClickableObject
             {
                 gameObject.transform.parent.position = hit.point;
             }
+            thisObstacle.enabled = false;
+        } else
+        {
+            thisObstacle.enabled = true;
         }
     }
 
