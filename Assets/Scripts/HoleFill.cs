827266e036600259e84f4b281b335b92dec68301
DragonHeart000 <rileyshumway@gmail.com>
2020-07-11 18:22:58 -0500

    Started working on hole

---
+++ Assets/Scripts/HoleFill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleFill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ClickableRock")
+            Debug.Log("ROCKY!!!");
+
+        Debug.Log("ye");
+    }
+}
