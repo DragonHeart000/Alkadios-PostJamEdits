7fcb66e6674a35920b3c8f034b656e11f48f9d66
DragonHeart000 <rileyshumway@gmail.com>
2020-07-10 19:07:13 -0500

    Added some pathfinding logic

---
+++ Assets/Scripts/Character/Movement/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeTarget()
+    {
+        changeTarget(TargetHub.instance.getRandom().gameObject.transform.position);
+    }
+
+    public void changeTarget(Vector3 newDestination)
+    {
+        agent.SetDestination(newDestination);
+    }
+}
