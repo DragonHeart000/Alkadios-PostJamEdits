54eefb48fe431dc666812218e770802e23496015
DragonHeart000 <rileyshumway@gmail.com>
2020-07-10 20:37:10 -0500

    Randomly selects spots now and goes there automatically. If a spot can not be gotten to it then chooses another

--- Assets/Scripts/Character/Movement/PlayerController.cs
+++ Assets/Scripts/Character/Movement/PlayerController.cs
@@ -10,22 +10,49 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        changeTillValidTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (agent.remainingDistance <= 0.2)
+        {
+            Debug.Log("Arrived!");
+            changeTillValidTarget();
+        }
     }
 
-    public void changeTarget()
+    //Change till valid target will keep trying to change the target randomly untill it finds a target it is able to do
+    public void changeTillValidTarget()
     {
-        changeTarget(TargetHub.instance.getRandom().gameObject.transform.position);
+        if (!changeTarget())
+        {
+            changeTillValidTarget();
+        }
     }
 
-    public void changeTarget(Vector3 newDestination)
+    //Change target will pick a random target to go to. Returns true if path is valid and false if it can not get there
+    public bool changeTarget()
     {
-        agent.SetDestination(newDestination);
+        return changeTarget(TargetHub.instance.getRandom().gameObject.transform.position);
+    }
+
+    //Change target vector3 will set the destination to a specific location. Returns true if path is valid and false if it can not get there
+    public bool changeTarget(Vector3 newDestination)
+    {
+        NavMeshPath path = new NavMeshPath();
+        agent.destination = newDestination;
+        agent.CalculatePath(newDestination, path);
+
+        if(path.status == NavMeshPathStatus.PathPartial || path.status == NavMeshPathStatus.PathInvalid)
+        {
+            Debug.Log("Invalid path");
+            return false;
+        } else
+        {
+            return true;
+        }
+        
     }
 }
