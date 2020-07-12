9e92dd6a4b11ca6b59a97d81c8c072332c922671
DragonHeart000 <rileyshumway@gmail.com>
2020-07-11 19:31:04 -0500

    Added victorabda's sound effects and implimented footsteps

---
+++ Assets/Scripts/Audio Controlls/Footsteps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    private float speed;
+    private Vector3 lastPosition;
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curMove = transform.position - lastPosition;
+        speed = curMove.magnitude / Time.deltaTime;
+        lastPosition = transform.position;
+
+        if (speed > 2f)
+        {
+            audio.mute = false;
+        } else
+        {
+            audio.mute = true;
+        }
+    }
+
+}
