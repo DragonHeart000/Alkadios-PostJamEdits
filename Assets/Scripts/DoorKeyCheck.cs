7789e4718e96036375a9b24f7422d029fbb1effd
DragonHeart000 <rileyshumway@gmail.com>
2020-07-12 03:10:36 -0500

    Fixed door animation

--- Assets/Scripts/DoorKeyCheck.cs
+++ Assets/Scripts/DoorKeyCheck.cs
@@ -24,7 +24,6 @@ public class DoorKeyCheck : MonoBehaviour
                 Debug.Log("OpenDoor");
                 ani.SetTrigger("open"); //this animation is not working
                 Destroy(blockerCube);
-                Destroy(gameObject);
             }
         }
     }
