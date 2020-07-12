f9bde2ef5a5f659e205d1e152a58d86167f3ca36
DragonHeart000 <rileyshumway@gmail.com>
2020-07-11 20:53:22 -0500

    Made it so you can fill in the hole

--- Assets/Scripts/HoleFill.cs
+++ Assets/Scripts/HoleFill.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class HoleFill : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public MeshRenderer meshRend;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private bool doIt = false; //Quick and dirty fix, don't do this unless pressed for time in a game jam
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "ClickableRock")
-            Debug.Log("ROCKY!!!");
-
-        Debug.Log("ye");
+        if (doIt)
+        {
+            //The only thing that could possibly enter is a rock so no need to check for it. You totally should check for it but GAME JAM
+            GameObject.Destroy(other.transform.parent.gameObject); //Kill the rock
+            Debug.Log("Why?");
+            meshRend.enabled = true;
+            TargetHub.instance.surface.BuildNavMesh();
+        } else
+        {
+            //Being inside the terrain triggers a OnTriggerEnter so we just ignore the first one.
+            doIt = !doIt;
+        }
+        
     }
 }
